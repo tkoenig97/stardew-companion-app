946f222ab48af85534c9b48267b9f50183ec4b10
tkoenig97 <tylerskoenig@gmail.com>
2023-04-12 17:04:11 -0500

    Connected Vite + React with Django

--- stardew_app/views.py
+++ stardew_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def react_index(request):
+    index = open('static/index.html')
+    return HttpResponse(index)
