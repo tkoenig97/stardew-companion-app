3c819981c898f8cdcdb4419f92f13cd73fd1e5ea
tkoenig97 <tylerskoenig@gmail.com>
2023-04-12 17:03:04 -0500

    Connected Vite + React with Django

--- stardew_react/vite.config.js
+++ stardew_react/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : '/static/',
+  build : {
+    outDir : '../static',
+    emptyOutDir : true,
+    sourcemap : true,
+  },
   plugins: [react()],
 })
