2a4fef7504d5b95a54d0db01c0319febf24f0c74
tkoenig97 <tylerskoenig@gmail.com>
2023-04-26 13:05:46 -0500

    Finished first iteration of NavBar

--- stardew_react/src/components/Navbar.jsx
+++ stardew_react/src/components/Navbar.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom"
+
 export const Navbar = () => {
     return (
         <div className="navbar">
-            <h1>Stardew Valley</h1>
+            <img className="navimg" src="src/assets/images/stardewchicken.png" />
+            <Link to={'/'}>Home</Link>
+            <Link to={'/'}>Page 1</Link>
+            <Link to={'/'}>Page 2</Link>
+            <Link to={'/'}>Page 3</Link>
+            <Link to={'/'}>Sign-In/Sign-Up</Link>
+            <form>
+                <input name="search" placeholder="Search"></input>
+                <button className='submit-button' type="submit">Search</button>
+            </form>
         </div>
     )
 }
