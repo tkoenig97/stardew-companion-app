d596845ee2c2ead33f9400cf9b6aac7acb401523
tkoenig97 <tylerskoenig@gmail.com>
2023-04-27 13:55:06 -0500

    Added index number to top corner of cell

--- stardew_react/src/components/DayCell.jsx
+++ stardew_react/src/components/DayCell.jsx
@@ -1,7 +1,7 @@
-export const DayCell = () => {
+export const DayCell = ({ index }) => {
     return (
         <div className="cell">
-            
+            <div className="day-number">{index + 1}</div>
         </div>
-    )
-}
+    );
+};
