34ea23cd401d6a470846c7d519cfffd59dd8fcde
tkoenig97 <tylerskoenig@gmail.com>
2023-04-27 15:12:14 -0500

    Clicking a day cell will add or remove an X

--- stardew_react/src/components/DayCell.jsx
+++ stardew_react/src/components/DayCell.jsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+
 export const DayCell = ({ day }) => {
+    const [checkedOff, setCheckedOff] = useState(false);
+
+    const handleClick = () => {
+        setCheckedOff(!checkedOff)
+    }
+
     return (
-        <div className="cell">
+        <div className="cell" onClick={handleClick}>
             <div className="day-number">{day}</div>
+            {checkedOff && <div className='x-mark'>X</div>}
         </div>
     );
 };
