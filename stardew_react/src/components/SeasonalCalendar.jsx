27245aeec1b1a590c34f9dcc629d40897d09260a
tkoenig97 <tylerskoenig@gmail.com>
2023-04-28 14:20:18 -0500

    Added ability to change seasonal calendar with correct data

--- stardew_react/src/components/SeasonalCalendar.jsx
+++ stardew_react/src/components/SeasonalCalendar.jsx
@@ -4,7 +4,9 @@ import { getSeasonalData } from '../utilities';
 import { capitalizeFirstLetter } from '../utilities';
 
 export const SeasonalCalendar = () => {
-    const [currSeason, setCurrSeason] = useState('spring');
+    const seasons = ['spring', 'summer', 'fall', 'winter'];
+    const [currSeasonIndex, setCurrSeasonIndex] = useState(0);
+    const [currSeason, setCurrSeason] = useState(seasons[currSeasonIndex]);
     const [seasonalData, setSeasonalData] = useState(
         getSeasonalData({ season: currSeason })
     );
@@ -12,6 +14,24 @@ export const SeasonalCalendar = () => {
     useEffect(() => {
         setSeasonalData(getSeasonalData({ season: currSeason }));
     }, [currSeason]);
+
+    useEffect(() => {
+        setCurrSeason(seasons[currSeasonIndex]);
+    }, [currSeasonIndex]);
+
+    // Cycle left through seasons array
+    const handleLeftClick = () => {
+        setCurrSeasonIndex(
+            currSeasonIndex === 0 ? seasons.length - 1 : currSeasonIndex - 1
+        );
+    };
+
+    // Cycle right through seasons array
+    const handleRightClick = () => {
+        setCurrSeasonIndex(
+            currSeasonIndex === seasons.length - 1 ? 0 : currSeasonIndex + 1
+        );
+    };
 
     // Generate an array of 28 DayCell components
     const daysArray = Array.from({ length: 28 }, (_, i) => (
@@ -23,14 +43,12 @@ export const SeasonalCalendar = () => {
         daysArray.slice(i * 7, (i + 1) * 7)
     );
 
-    console.log(seasonalData);
-
     return (
         <>
             <div className="calendar-header">
-                <button>Left Button</button>
+                <button onClick={handleLeftClick}>Left Button</button>
                 <h1>{capitalizeFirstLetter(currSeason)}</h1>
-                <button>Right Button</button>
+                <button onClick={handleRightClick}>Right Button</button>
             </div>
             <div className="calendar">
                 <div className="days-label">
