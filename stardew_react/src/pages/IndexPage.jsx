0eeb2bddaf24b1f931a24d394fac299799c66d1d
tkoenig97 <tylerskoenig@gmail.com>
2023-04-29 01:10:43 -0500

    Fixed custom artwork

--- stardew_react/src/pages/IndexPage.jsx
+++ stardew_react/src/pages/IndexPage.jsx
@@ -2,6 +2,7 @@ export const IndexPage = () => {
     return (
         <div className="menu page-content">
             <h1>Index Page</h1>
+            <img className="index-img" src="src/assets/images/brownchicken.png" />
         </div>
     )
 }
