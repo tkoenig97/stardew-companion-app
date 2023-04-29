af55447bb968ec7b6064894934fef33e9a4a7132
tkoenig97 <tylerskoenig@gmail.com>
2023-04-29 16:06:57 -0500

    Added the rest of the calendar sprites

--- stardew_react/src/utilities.jsx
+++ stardew_react/src/utilities.jsx
@@ -3,25 +3,25 @@ import Star from '../src/assets/images/calendar/calendar-star.png'
 import Vincent from '../src/assets/images/calendar/calendar-vincent.png'
 import Caroline from '../src/assets/images/calendar/calendar-vincent.png'
 import Kent from '../src/assets/images/calendar/calendar-kent.png'
-// import Lewis from '../src/assets/images/calendar/calendar-lewis.png'
+import Lewis from '../src/assets/images/calendar/calendar-lewis.png'
 import Haley from '../src/assets/images/calendar/calendar-haley.png'
 import Pam from '../src/assets/images/calendar/calendar-pam.png'
 import Shane from '../src/assets/images/calendar/calendar-shane.png'
-// import Pierre from '../src/assets/images/calendar/calendar-pierre.png'
+import Pierre from '../src/assets/images/calendar/calendar-pierre.png'
 import Emily from '../src/assets/images/calendar/calendar-emily.png'
 import Jas from '../src/assets/images/calendar/calendar-jas.png'
-// import Gus from '../src/assets/images/calendar/calendar-gus.png'
+import Gus from '../src/assets/images/calendar/calendar-gus.png'
 import Maru from '../src/assets/images/calendar/calendar-maru.png'
 import Alex from '../src/assets/images/calendar/calendar-alex.png'
 import Sam from '../src/assets/images/calendar/calendar-sam.png'
 import Demetrius from '../src/assets/images/calendar/calendar-demetrius.png'
-// import Dwarf from '../src/assets/images/calendar/calendar-dwarf.png'
-// import Willy from '../src/assets/images/calendar/calendar-willy.png'
-// import Penny from '../src/assets/images/calendar/calendar-penny.png'
+import Dwarf from '../src/assets/images/calendar/calendar-dwarf.png'
+import Willy from '../src/assets/images/calendar/calendar-willy.png'
+import Penny from '../src/assets/images/calendar/calendar-penny.png'
 import Elliott from '../src/assets/images/calendar/calendar-elliott.png'
 import Jodi from '../src/assets/images/calendar/calendar-jodi.png'
 import Abigail from '../src/assets/images/calendar/calendar-abigail.png'
-// import Sandy from '../src/assets/images/calendar/calendar-sandy.png'
+import Sandy from '../src/assets/images/calendar/calendar-sandy.png'
 import Marnie from '../src/assets/images/calendar/calendar-marnie.png'
 import Robin from '../src/assets/images/calendar/calendar-robin.png'
 import George from '../src/assets/images/calendar/calendar-george.png'
@@ -39,34 +39,34 @@ export const getSeasonalData = ({ season }) => {
     const seasonalData = {
         spring: {
             4: Kent,
-            7: 'Lewis',
+            7: Lewis,
             10: Vincent,
             13: Flag,
             14: Haley,
             18: Pam,
             20: Shane,
             24: Flag,
-            26: 'Pierre',
+            26: Pierre,
             27: Emily,
         },
         summer: {
             4: Jas,
-            8: 'Gus',
+            8: Gus,
             10: Maru,
             11: Flag,
             13: Alex,
             17: Sam,
             19: Demetrius,
-            22: 'Dwarf',
-            24: 'Willy',
+            22: Dwarf,
+            24: Willy,
             29: Flag,
         },
         fall: {
-            2: 'Penny',
+            2: Penny,
             5: Elliott,
             11: Jodi,
             13: Abigail,
-            15: 'Sandy',
+            15: Sandy,
             16: Flag,
             18: Marnie,
             21: Robin,
