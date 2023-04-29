ffe923dc98319ece5438bc53ea8ea35c9cb5005d
tkoenig97 <tylerskoenig@gmail.com>
2023-04-29 09:53:20 -0500

    Added current sprites

--- stardew_react/src/utilities.jsx
+++ stardew_react/src/utilities.jsx
@@ -1,55 +1,89 @@
-import Vincent from '../src/assets/images/calendar-vincent.png'
+import Vincent from '../src/assets/images/calendar/calendar-vincent.png'
+import Caroline from '../src/assets/images/calendar/calendar-vincent.png'
+import Kent from '../src/assets/images/calendar/calendar-kent.png'
+// import Lewis from '../src/assets/images/calendar/calendar-lewis.png'
+import Event from '../src/assets/images/calendar/calendar-flag.png'
+import Haley from '../src/assets/images/calendar/calendar-haley.png'
+import Pam from '../src/assets/images/calendar/calendar-pam.png'
+import Shane from '../src/assets/images/calendar/calendar-shane.png'
+// import Pierre from '../src/assets/images/calendar/calendar-pierre.png'
+import Emily from '../src/assets/images/calendar/calendar-emily.png'
+import Jas from '../src/assets/images/calendar/calendar-jas.png'
+// import Gus from '../src/assets/images/calendar/calendar-gus.png'
+import Maru from '../src/assets/images/calendar/calendar-maru.png'
+import Alex from '../src/assets/images/calendar/calendar-alex.png'
+import Sam from '../src/assets/images/calendar/calendar-sam.png'
+import Demetrius from '../src/assets/images/calendar/calendar-demetrius.png'
+// import Dwarf from '../src/assets/images/calendar/calendar-dwarf.png'
+// import Willy from '../src/assets/images/calendar/calendar-willy.png'
+// import Penny from '../src/assets/images/calendar/calendar-penny.png'
+import Elliott from '../src/assets/images/calendar/calendar-elliott.png'
+import Jodi from '../src/assets/images/calendar/calendar-jodi.png'
+import Abigail from '../src/assets/images/calendar/calendar-abigail.png'
+// import Sandy from '../src/assets/images/calendar/calendar-sandy.png'
+import Marnie from '../src/assets/images/calendar/calendar-marnie.png'
+import Robin from '../src/assets/images/calendar/calendar-robin.png'
+import George from '../src/assets/images/calendar/calendar-george.png'
+import Krobus from '../src/assets/images/calendar/calendar-krobus.png'
+import Linus from '../src/assets/images/calendar/calendar-linus.png'
+import Sebastian from '../src/assets/images/calendar/calendar-sebastian.png'
+import Harvey from '../src/assets/images/calendar/calendar-harvey.png'
+import Wizard from '../src/assets/images/calendar/calendar-wizard.png'
+import Evelyn from '../src/assets/images/calendar/calendar-evelyn.png'
+import Leah from '../src/assets/images/calendar/calendar-leah.png'
+import Clint from '../src/assets/images/calendar/calendar-clint.png'
+
 
 export const getSeasonalData = ({ season }) => {
     const seasonalData = {
         spring: {
-            4: 'Kent',
+            4: Kent,
             7: 'Lewis',
             10: Vincent,
-            13: 'Event',
-            14: 'Haley',
-            18: 'Pam',
-            20: 'Shane',
-            24: 'Event',
+            13: Event,
+            14: Haley,
+            18: Pam,
+            20: Shane,
+            24: Event,
             26: 'Pierre',
-            27: 'Emily',
+            27: Emily,
         },
         summer: {
-            4: 'Jas',
+            4: Jas,
             8: 'Gus',
-            10: 'Maru',
-            11: 'Event',
-            13: 'Alex',
-            17: 'Sam',
-            19: 'Demetrius',
+            10: Maru,
+            11: Event,
+            13: Alex,
+            17: Sam,
+            19: Demetrius,
             22: 'Dwarf',
             24: 'Willy',
-            29: 'Event',
+            29: Event,
         },
         fall: {
             2: 'Penny',
-            5: 'Elliott',
-            11: 'Jodi',
-            13: 'Abigail',
+            5: Elliott,
+            11: Jodi,
+            13: Abigail,
             15: 'Sandy',
-            16: 'Event',
-            18: 'Marnie',
-            21: 'Robin',
-            24: 'George',
-            27: 'Event',
+            16: Event,
+            18: Marnie,
+            21: Robin,
+            24: George,
+            27: Event,
         },
         winter: {
-            1: 'Krobus',
-            3: 'Linus',
-            7: 'Caroline',
-            8: 'Event',
-            10: 'Sebastian',
-            14: 'Harvey',
-            17: 'Wizard',
-            20: 'Evelyn',
-            23: 'Leah',
-            25: 'Event',
-            26: 'Clint',
+            1: Krobus,
+            3: Linus,
+            7: Caroline,
+            8: Event,
+            10: Sebastian,
+            14: Harvey,
+            17: Wizard,
+            20: Evelyn,
+            23: Leah,
+            25: Event,
+            26: Clint,
         },
     };
     return seasonalData[season]
