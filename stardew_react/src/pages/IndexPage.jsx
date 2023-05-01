3a43c4c8ad5a0fce90d3ccb519ef6465f7e60234
tkoenig97 <tylerskoenig@gmail.com>
2023-05-01 12:48:21 -0500

    First interation index page

--- stardew_react/src/pages/IndexPage.jsx
+++ stardew_react/src/pages/IndexPage.jsx
@@ -1,8 +1,21 @@
 export const IndexPage = () => {
     return (
         <div className="menu page-content">
-            <h1>Index Page</h1>
+            <h1>Welcome!</h1>
             <img className="index-img" src="src/assets/images/brown-chicken.png" />
+            <p className="index-paragraph">
+                Welcome to our Stardew Valley helper website! Our site is
+                designed to provide you with all the tools and information you
+                need to become a successful farmer in Stardew Valley. From a
+                comprehensive calendar that shows all the important events
+                throughout the year, to a database of villagers' likes and
+                dislikes, and information on every crop, foragable, and animal
+                product in the game, we've got you covered. Our goal is to make
+                your time in Stardew Valley as efficient and enjoyable as
+                possible, by providing you with all the information you need in
+                one convenient location.
+            </p>
+            <footer>Check us out on <a className="github-link" href="https://github.com/tkoenig97/stardew-companion-app">Github</a></footer>
         </div>
-    )
-}
+    );
+};
