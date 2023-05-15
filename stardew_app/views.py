9d00c8103785438df2f30d4d96268b225069327d
tkoenig97 <tylerskoenig@gmail.com>
2023-05-15 14:13:57 -0500

    Set up App_User and api views

--- stardew_app/views.py
+++ stardew_app/views.py
@@ -1,6 +1,70 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.decorators import api_view
+from .models import App_User
+from django.core.serializers import serialize
 
 # Create your views here.
+@api_view(["POST"])
+def user_sign_up(request):
+    email = request.data['email']
+    password = request.data['password']
+    name = request.data['name']
+    super_user = False
+    staff = False
+    if 'super' in request.data:
+        super_user = request.data['super']
+    if 'staff' in request.data:
+        staff = request.data['staff']
+    try:
+        # creates new user
+        new_user = App_User.objects.create_user(username = email, email = email, name = name, password = password, is_superuser = super_user, is_staff = staff)
+        new_user.save()
+        return JsonResponse({"success":True})
+    except Exception as e:
+        print(e)
+        return JsonResponse({"success": False})
+
+@api_view(["POST"])
+def user_log_in(request):
+
+    email = request.data['email']
+    password = request.data['password']
+    user = authenticate(username = email , password = password)
+    if user is not None and user.is_active:
+        try:
+            # Creates SessionID
+            login(request._request, user)
+            print(user)
+            return JsonResponse({'email': user.email, 'name':user.name})
+        except Exception as e:
+            print(e)
+            return JsonResponse({'login':False})
+    return JsonResponse({'login':False})
+
+
+@api_view(["GET"])
+def curr_user(request):
+
+    if request.user.is_authenticated:
+        #                    format       query                     options
+        user_info = serialize("json",  [request.user], fields = ['name', 'email'])
+        user_info_workable = json.loads(user_info)
+        return JsonResponse(user_info_workable[0]['fields'])
+    else:
+        return JsonResponse({"user":None})
+
+@api_view(['POST'])
+def user_log_out(request):
+    try:
+        # Removes SessionID
+        logout(request)
+        return JsonResponse({'logout':True})
+    except Exception as e:
+        print(e)
+        return JsonResponse({'logout':False})
+
 def react_index(request):
+    # Grabs index from React app
     index = open('static/index.html')
     return HttpResponse(index)
