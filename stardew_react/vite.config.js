18c108d55f21bdafa7da7dda438ce802d0d1e576
tkoenig97 <tylerskoenig@gmail.com>
2023-05-15 12:43:54 -0500

    Set up watcher

--- stardew_react/vite.config.js
+++ stardew_react/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base : '/static/',
-  // build : {
-  //   outDir : '../static',
-  //   emptyOutDir : true,
-  //   sourcemap : true,
-  // },
+  base : '/static/',
+  build : {
+    outDir : '../static',
+    emptyOutDir : true,
+    sourcemap : true,
+  },
   plugins: [react()],
 })
